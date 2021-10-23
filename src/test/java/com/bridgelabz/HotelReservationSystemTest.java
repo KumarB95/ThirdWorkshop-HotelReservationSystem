566577eae09258ac0d3d1092adf2427fead497b7
KumarB95 <kumarbalingal@gmail.com>
2021-10-23 09:02:13 +0530

    Added : Added three hotels in the list and passed details of rates

---
+++ src/test/java/com/bridgelabz/HotelReservationSystemTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HotelReservationSystemTest {
+    HotelReservationSystem Obj = new HotelReservationSystem();
+
+    @Test
+    void checkAllThreeHotelsAreAdded(){
+        Obj.addHotelDetails();
+        Assertions.assertEquals( 3, Obj.hotelList.size());
+    }
+}
