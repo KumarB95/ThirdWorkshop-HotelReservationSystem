9d277efc1a4b91a1d08a9f8b17b27de417691d5e
KumarB95 <kumarbalingal@gmail.com>
2021-10-27 22:43:53 +0530

    Refactored : Added weekday and weekend rates for all three hotels

--- src/test/java/com/bridgelabz/HotelReservationSystemTest.java
+++ src/test/java/com/bridgelabz/HotelReservationSystemTest.java
@@ -4,15 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HotelReservationSystemTest {
-    HotelReservationSystem Obj = new HotelReservationSystem();
+    HotelReservationSystem hotelReservation = new HotelReservationSystem();
+    @Test
+    public void add_Hotels() {
+        Assertions.assertTrue(hotelReservation.addHotel(new Hotel("LakeWood", "LakeWood", 110, 110)));
+        Assertions.assertTrue(hotelReservation.addHotel(new Hotel("LakeWood", "Bridgewood", 110, 160)));
+        Assertions.assertTrue(hotelReservation.addHotel(new Hotel("LakeWood", "Ridgewood", 110, 220)));
+    }
 
     @Test
-    void checkAllThreeHotelsAreAdded(){
-        Obj.addHotelDetails();
-        Assertions.assertEquals( 3, Obj.hotelList.size());
+    public void toFindTheCheapestHotel() {
+        HotelReservationSystem cheapHotel = new HotelReservationSystem();
+        cheapHotel.findCheapestHotel();
     }
     @Test
-    void checkCheapestHotel(){
-        Obj.findCheapestHotel();
+    public void addWeekdayAndWeekendRate() {
+        HotelReservationSystem weekdayWeekend = new HotelReservationSystem();
+        weekdayWeekend.weekday_weekend_rate();
     }
 }
